bc739e0baec8e33d1499449f2bad8b07d55d2c72
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-06-16 14:32:55 -0300

    Modificacao
    Modificacao

--- src/main/java/projetointegrador/MainApp2.java
+++ src/main/java/projetointegrador/MainApp2.java
@@ -7,6 +7,11 @@ public class MainApp2 {
 
 	public static void main(String[] args) {
 		Produto produto = Produto.builder().id(1).nome("Dembele").descricao("nao aceitamos pet").tipo(TipoEnum.BASICO).build();
+		Produto produto1 = Produto.builder().id(1).nome("Dembele").descricao("nao aceitamos pet").tipo(TipoEnum.BASICO).build();
+		Produto produto2 = Produto.builder().id(1).nome("Dembele").descricao("nao aceitamos pet").tipo(TipoEnum.BASICO).build();
+		Produto produto3 = Produto.builder().id(1).nome("Dembele").descricao("nao aceitamos pet").tipo(TipoEnum.BASICO).build();
+		Produto produto4 = Produto.builder().id(1).nome("Dembele").descricao("nao aceitamos pet").tipo(TipoEnum.BASICO).build();
+
 	}
 
 }
